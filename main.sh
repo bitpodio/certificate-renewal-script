135115e62f3940839a30c5e07da77ca0ca442322
Adarsh Singh <adarsh6162singh@gmail.com>
2021-08-23 19:23:23 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,11 +6,9 @@ certbot certonly \
   --dns-google \
   --dns-google-credentials /opt/certbot/dns/google.json \
   --dns-google-propagation-seconds 30 \
-  -d *.gfive.workpay.io \
-  -d *.lcv.workpay.io
+  -d *.star.workpay.io \
+  -d *.mvc.workpay.io
 
-gcloud compute ssl-certificates create mynewcert --certificate /etc/letsencrypt/live/gfive.workpay.io/fullchain.pem --private-key /etc/letsencrypt/live/gfive.workpay.io/privkey.pem
+gcloud compute ssl-certificates create $CERT_NAME --certificate /etc/letsencrypt/live/$CERT_PATH/fullchain.pem --private-key /etc/letsencrypt/live/$CERT_PATH/privkey.pem
 
-gcloud compute target-https-proxies update bitpod-test-lb-target-proxy-3 --ssl-certificates mynewcert
-
-gcloud compute target-https-proxies update bitpod-lb-production-target-proxy-2 --ssl-certificates mynewcert
+gcloud compute target-https-proxies update bitpod-test-lb-target-proxy-3 --ssl-certificates $CERT_NAME
