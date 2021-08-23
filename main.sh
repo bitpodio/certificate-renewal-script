d85c12846e31d0b73e39fe5841a479aacbd35bab
Adarsh Singh <adarsh6162singh@gmail.com>
2021-08-23 22:22:34 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-gcloud auth activate-service-account cert-renew-patch-lb@bitpodtest.iam.gserviceaccount.com --key-file=/opt/certbot/dns/google.json --project=bitpodtest
+gcloud auth activate-service-account cert-renew-patch-lb@bitpodtest.iam.gserviceaccount.com --key-file=/opt/certbot/dns/google.json --project=bitpodtest &&
 
 certbot certonly \
   --dns-google \
   --dns-google-credentials /opt/certbot/dns/google.json \
   --dns-google-propagation-seconds 30 \
   -d *.star.workpay.io \
-  -d *.mvc.workpay.io
+  -d *.mvc.workpay.io --register-unsafely-without-email &&
 
-gcloud compute ssl-certificates create bitpod-cert-$(printf '%(%Y-%m-%d)T\n' -1) --certificate /etc/letsencrypt/live/$CERT_PATH/fullchain.pem --private-key /etc/letsencrypt/live/$CERT_PATH/privkey.pem
+gcloud compute ssl-certificates create bitpod-cert-$(printf '%(%Y-%m-%d)T\n' -1) --certificate /etc/letsencrypt/live/$CERT_PATH/fullchain.pem --private-key /etc/letsencrypt/live/$CERT_PATH/privkey.pem &&
 
 gcloud compute target-https-proxies update bitpod-test-lb-target-proxy-3 --ssl-certificates bitpod-cert-$(printf '%(%Y-%m-%d)T\n' -1)
