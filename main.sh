76f0d72cdbf2e66fa9688786c26a8912f67ad653
Adarsh Singh <adarsh6162singh@gmail.com>
2021-08-23 22:40:13 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -7,7 +7,7 @@ certbot certonly \
   --dns-google-credentials /opt/certbot/dns/google.json \
   --dns-google-propagation-seconds 30 \
   -d *.star.workpay.io \
-  -d *.mvc.workpay.io --register-unsafely-without-email &&
+  -d *.mvc.workpay.io --register-unsafely-without-email --non-interactive --agree-tos &&
 
 gcloud compute ssl-certificates create bitpod-cert-$(printf '%(%Y-%m-%d)T\n' -1) --certificate /etc/letsencrypt/live/$CERT_PATH/fullchain.pem --private-key /etc/letsencrypt/live/$CERT_PATH/privkey.pem &&
 
